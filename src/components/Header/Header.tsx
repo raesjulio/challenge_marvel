a6597476854d388ac619d3fe712c5c0d0ac3546e
Julio <despairnatural@gmail.com>
2022-04-04 18:29:21 -0300

    svg backgroud

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,29 @@
 // import logoMarvel from "../../assets/logo.svg"
 import logo from "../../assets/logo.svg"
-import marvel from "../../assets/marvel.svg"
+import backgroud from "../../assets/backgroud.svg"
 import group from "../../assets/group.svg"
-
 import styles from "./styles.module.scss"
+import { useNavigate } from "react-router-dom"
 
 export const Header = () => {
+  let navigate = useNavigate();
+  const handleTeam = ()=>{
+    return navigate("/yourteam")
+  }
+  const handleHome = ()=>{
+    return navigate("/")
+  }
   return (
     <header className={styles.container}> 
       <div className={styles.content}>
-        <a href="#">
+        <a href="" onClick={handleHome}>
           <img  src={`${logo}`} alt="image logo ironman" />
           <span  className={styles.nameMarvel}>Marvel Strike Team</span>
         </a>
-        <button>Your Team <img  src={`${group}`} alt="img group" /></button>
+        <button type="button" onClick={handleTeam}>Your Team <img  src={`${group}`} alt="img group" /></button>
+      </div>
+      <div>
+        <img src={`${backgroud}`} alt="" />
       </div>
     </header>
   )
