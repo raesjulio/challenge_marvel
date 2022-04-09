d25df321756e74f171e7a89d287e27beba875e10
Julio <despairnatural@gmail.com>
2022-04-09 00:47:50 -0300

    interface variable

--- custom.d.ts
+++ custom.d.ts
@@ -7,3 +7,12 @@ declare module '*.scss' {
     const content: Record<string, string>;
     export default content;
 }
+interface ImportMetaEnv {
+    readonly VITE_API_KEY_PUBLIC: string
+    readonly VITE_API_KEY_SECRET: string
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
